58bfac365839added1191f89bb64c73707f443c4
kin-cunico <124842064+kin-cunico@users.noreply.github.com>
2023-07-15 10:55:01 +0100

    navbar responsive added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "@/styles/Navbar.module.css";
 import Link from "next/link";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+	const navRef = useRef();
+	const showNavbar = () => {
+		navRef.current.classList.toggle(`${styles.responsive_nav}`);
+	};
 	return (
-		<nav className={styles.navbar}>
-			<Link href="/">
-				<h1 className={styles.h1}>Luminae Wikia</h1>
-			</Link>
-			<ul className={styles.linkList}>
+		<header className={styles.smallHeader}>
+			<button
+				onClick={showNavbar}
+				className={`${styles.nav_btn} ${styles.nav_close_btn}`}
+			>
+				<FaBars />
+			</button>
+			<nav
+				className={styles.navbar}
+				ref={navRef}
+			>
+				<Link href="/">
+					<h1 className={styles.h1}>Luminae Wikia</h1>
+				</Link>
+				<Link href="/">
+					<li className={styles.link}>HOME</li>
+				</Link>
 				<Link href="#fauna">
 					<li className={styles.link}>FAUNA</li>
 				</Link>
@@ -24,8 +41,8 @@ const Navbar = () => {
 				>
 					<li className={styles.link}>CONTACT</li>
 				</Link>
-			</ul>
-		</nav>
+			</nav>
+		</header>
 	);
 };
 
